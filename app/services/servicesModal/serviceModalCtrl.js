ffe19a0ebc7ec115b5aec2c805c2af411d395e09
Evan Simons <evan.simons@gmail.com>
2014-08-13 01:13:41 -0400

    Tweaks.

--- app/services/servicesModal/serviceModalCtrl.js
+++ app/services/servicesModal/serviceModalCtrl.js
@@ -2,13 +2,6 @@
  * Created by Evan on 7/27/2014.
  */
 angular.module('easy-slp-scheduler')
-    .controller('newServiceModal', function($scope){
-        $scope.class = new Class();
-
-        function Class(){
-            this.name = null;
-            this.teacher = null;
-            this.students = [];
-            this.constraints = [];
-        }
+    .controller('newServiceModal', function($scope, caseloadService){
+        $scope.class = new caseloadService.Service();
     });
--- app/shared/testdataSrvc.js
+++ app/shared/testdataSrvc.js
@@ -5,11 +5,11 @@ angular.module('easy-slp-scheduler')
 .service('testdataService', function(caseloadService){
 
         caseloadService.students.list.push(new caseloadService.Student('Evan', 'Simons'));
-        caseloadService.students.list.push({firstName: 'Lauren', lastName: 'Simons', serviceReqs: [], serviceAppts: []});
-        caseloadService.students.list.push({firstName: 'Elliot', lastName: 'Simons', serviceReqs: [], serviceAppts: []});
+        caseloadService.students.list.push(new caseloadService.Student('Lauren', 'Simons'));
+        caseloadService.students.list.push(new caseloadService.Student('Elliot', 'Simons'));
 
-        caseloadService.services.list.push({name: 'Articulation'});
-        caseloadService.services.list.push({name: 'Language'});
+        caseloadService.services.list.push(new caseloadService.Service('Articulation'));
+        caseloadService.services.list.push(new caseloadService.Service('Language'));
 
         caseloadService.classes.list.push({name: 'First Grade', teacher: 'Mr. F', constraints: []});
     });
